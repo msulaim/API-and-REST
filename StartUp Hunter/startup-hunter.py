b57e48dddb0067ee3c35bc4fce886037d3dbd7f4
msulaim <msulaim@purdue.edu>
2021-04-28 16:13:14 +0500

    Added working code for Windows Goal 1.a)

--- StartUp Hunter/startup-hunter.py
+++ StartUp Hunter/startup-hunter.py
@@ -8,33 +8,60 @@
      b) Dig into a specific startup and find out everything new there is to know about it
 
 ->ToDo:
-    a) Build a function that takes a url as parameter, and captures a screenshot of a website and saves it in the current director with the filename being "[website name]-[date].jpg"
+    a) Build a function that takes a url as parameter, and saves pdf of a website and saves it in the current director with the filename being "[website name]-[date].pdf"
     b) Build a function that takes a url as parameter, and searches crunchbase.com for that website and finds out and prints everything we can know from crunchbase about that company
 
+->Done:
+    a) Only for Windows
+    
 ->Imports:
 
 '''
 
 import time
-from datetime import date
+from datetime import date 
 import pdfkit
+import os
+import platform
+import wkhtmltopdf
 
-
-
-
-
-def html_to_pdf(url):
+def html_to_pdf(url, name):
     '''
        ->Purpose: The following function converts html to pdf
+       ->Input: url ~ string
+                name ~ string
        ->Output: pdf file containing screenshot of website front page
     '''
-    path_wkhtmltopdf = r"C:\Users\mhsha\anaconda3\Lib\site-packages\wkhtmltopdf\bin\wkhtmltopdf.exe"
-    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-    pdfkit.from_url(url, "tesla.pdf", configuration=config)
+    #Today's date
+    today = date.today()
+    today = today.strftime("%m-%d-%y")
     
+    #Name of Outfile
+    outfile = '['+name+']'+'['+today+']'+'.pdf'
+    
+    #Install the binary file (.exe, .pkg)
+    #For Windows it will create a folder called 'wkhtmltopdf' in C:\Program Files, copy the folder to the directroy in which code is present
+    
+    # ~ Need to determine installation for Mac
+    
+    #For Windows, path is to an .exe
+    if platform.system() == 'Windows':
+        path = os.path.join(os.getcwd(),'wkhtmltopdf','bin','wkhtmltopdf.exe')
+    
+    #For Mac, path is to a .pkg
+    elif platform.system() == 'Darwin':
+        path = os.path.join(os.getcwd(),'wkhtmltopdf','bin','wkhtmltopdf.pkg')
+    
+    config = pdfkit.configuration(wkhtmltopdf=path)
+    
+    try:
+        pdfkit.from_url(url, outfile, configuration=config)   
+    except pdfkit.HostNotFoundError:
+        print('Invalid Website Name')
+
 
 if __name__ == "__main__":
     name = input('Enter name of website, no need for https:// or www. or .com \n-> eg: pinterest: ')
     url = 'https://www.' + name + '.com/'
-    html_to_pdf(url)
+    html_to_pdf(url, name)
     
